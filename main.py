ebf377db7d10799cccea83ff58f4b9875534370b
Nooblybr <166092409+Nooblybr@users.noreply.github.com>
2024-10-21 15:15:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from math import sqrt
+from re import split
 
 class Calculadora:
     def __init__(self,janela):
@@ -15,22 +17,30 @@ class Calculadora:
         self.frame_botoes = tk.Frame(janela)
         self.frame_botoes.pack()
 
-        botoes = [("7",0,0), ("8",0,1), ("9",0,2), ("/",0,3),
-                  ("4",1,0), ("5",1,1), ("6",1,2), ("*",1,3),
-                  ("1",2,0), ("2",2,1), ("3",2,2), ("-",2,3),
-                  ("0",3,0), (".",3,1), ("=",3,2), ("+",3,3)]
+        botoes = [("%",0,0), ("√",0,1), ("(",0,2), (")",0,3),
+                  ("7",1,0), ("8",1,1), ("9",1,2), ("/",1,3),
+                  ("4",2,0), ("5",2,1), ("6",2,2), ("*",2,3),
+                  ("1",3,0), ("2",3,1), ("3",3,2), ("-",3,3),
+                  ("0",4,0), (".",4,1), ("=",4,2), ("+",4,3),
+                  ("C",5,0)]
         
         for botao in botoes:
             self.criar_botoes(botao[0],botao[1],botao[2])
 
 
     def criar_botoes(self, texto, linha, coluna):
-        botao = tk.Button(self.frame_botoes,text=texto,font="Arial 12",width=6,height=3,command=lambda: self.apertar_botao(texto))
+        botao = tk.Button(self.frame_botoes,text=texto,font="Arial 12",width=7,height=3,command=lambda: self.apertar_botao(texto))
         botao.grid(row=linha, column=coluna)
 
     def apertar_botao(self,texto):
         if texto == "=":
             self.calcular()
+        elif texto == "√":
+            self.raiz_quadrada()
+        elif texto == "C":
+            self.limpar_visor()
+        elif texto == "%":
+            self.porcentagem()
         else:
             self.expressao += texto
             self.texto.set(self.expressao)
@@ -38,13 +48,43 @@ class Calculadora:
         
     def calcular(self):
         expressao = self.texto.get()
-        resultado = eval(expressao)
+        resultado = eval(expressao,)
         self.expressao = str(resultado)
+        self.texto.set(self.expressao)
+
+    def raiz_quadrada(self):
+        expressao = self.expressao
+        try:
+            resultado = sqrt(eval(expressao))
+            self.expressao = str(resultado)
+            self.texto.set(self.expressao)
+        except:
+            self.texto.set("ERRO")
+            self.expressao = ""
+
+    def porcentagem(self):
+        expressao = self.texto.get()
+        elemento = split(r"[/*\-+]", expressao)
+        porcentagem = float(elemento[-1])/100
+        expressao_simples = expressao[0:len(expressao)-len(elemento[-1])-1]
+        calculo_porcentagem = float(eval(expressao_simples) * porcentagem)
+        expressao = expressao[0:len(expressao)-len(elemento[-1])]+str(calculo_porcentagem)
+
+        resultado = eval(expressao)
+
+        self.texto.set(str(resultado))
+        self.expressao = str(resultado)
+
+
+
+
+    def limpar_visor(self):
+        self.expressao=""
         self.texto.set(self.expressao)
 
 janela = tk.Tk()
 janela.title("Calculadora")
-janela.geometry("400x300")
+janela.geometry("300x552")
 
 calculadora = Calculadora(janela)
 
